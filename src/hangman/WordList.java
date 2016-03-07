1c390af37e2650c3b4b5f72c851e3ee63d1bba79
dbrowncode <dbrownmedia@gmail.com>
2016-03-06 20:58:35 -0800

    Added "play again" + session scoring, comments, various fixes

--- src/hangman/WordList.java
+++ src/hangman/WordList.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Allows hangman game to get a list of words from an external file to use as its dictionary.
+ * Designed to work with a newline-separated list of words.
  */
 package hangman;
 
@@ -13,33 +12,65 @@ import java.util.Random;
 
 /**
  *
- * @author meisb
+ * @author Dustin Brown
+ * @since March 5, 2016
  */
 public class WordList {
     
     private URL path;
     private int numLines;
 
+    /**
+     * Constructor taking a path to a file.
+     *
+     * @param filepath - path to file.
+     */
     public WordList(URL filepath) {
         this.path = filepath;
     } 
 
+    /**
+     * Get the path of the file used by the WordList.
+     *
+     * @return path - path to file.
+     */
     public URL getPath() {
         return path;
     }
 
+    /**
+     * Set the path.
+     *
+     * @param filepath - path to file.
+     */
     public void setPath(URL filepath) {
         this.path = filepath;
     }
 
+    /**
+     * Get the number of lines in the file.
+     *
+     * @return numLines - number of lines.
+     */
     public int getNumLines() {
         return numLines;
     }
 
+    /**
+     * Set the number of lines expected in the file.
+     *
+     * @param num - number of lines.
+     */
     public void setNumLines(int num) {
         this.numLines = num;
     }
     
+    /**
+     * Get an array of all the words (lines) in the file.
+     *
+     * @return words - array of words.
+     * @throws IOException
+     */
     public String[] getWords() throws IOException{
         this.numLines = countLines();
         String[] words = new String[this.numLines];
@@ -54,6 +85,12 @@ public class WordList {
         return words;
     }
    
+    /**
+     * Find out how many lines/words are in the file.
+     *
+     * @return lineCount - number of lines.
+     * @throws IOException
+     */
     public int countLines() throws IOException {        
         BufferedReader br = new BufferedReader(new InputStreamReader(path.openStream()));
 
@@ -66,6 +103,12 @@ public class WordList {
         return lineCount;
     }
     
+    /**
+     * Get a random word from the array of words.
+     *
+     * @return words[i] - random word.
+     * @throws IOException
+     */
     public String selectWord() throws IOException {
         String[] words = getWords();
         Random rng = new Random();
